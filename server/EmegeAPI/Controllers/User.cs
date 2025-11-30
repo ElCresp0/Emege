145022b06b0ce0cf0f304c11d4487e922ba66905
ElCresp0 <jacky8203@protonmail.com>
2025-11-30 20:52:10 +0100

    add dummy user endpoints

--- server/EmegeAPI/Controllers/User.cs
+++ server/EmegeAPI/Controllers/User.cs
@@ -1,14 +1,31 @@
 using EmegeAPI.Models;
+using EmegeAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EmegeAPI.Controllers;
 
 public class UserController : Controller
 {
-    // GET: /user/
-    public UserModel getUser(string name)
+    // 
+    // This class defines routes and endpoints regarding application users.
+    // Actual endpoint logic is implemented in a corresponding UserService class.
+    // TODO: enhance swagger documentation
+    // 
+
+    // GET: /users/{name}
+    [HttpGet("/users/{name}")]
+    public ActionResult getUser(string name)
     {
-        // TODO: Attach a DB and create a user
-        return new UserModel(id: 5, name: name);
+        UserModel? user = UserService.GetUser(name);
+        return (user != null) ? Ok(user) : NotFound($"Could not find user: {name}");
+    }
+
+    // POST: /users/register
+    [HttpPost("/users/register")]
+    public ActionResult registerUser(string name)
+    {
+        UserModel? user = UserService.CreateUser(name);
+        return (user != null) ? Created($"/users/{name}", user) :
+            BadRequest($"Registration unsuccessful, wrong name: {name}");
     }
 }
--- server/EmegeAPI/Models/User.cs
+++ server/EmegeAPI/Models/User.cs
@@ -2,12 +2,12 @@ namespace EmegeAPI.Models;
 
 public class UserModel
 {
-    public UserModel(long id, string name)
+    public UserModel(Guid id, string name)
     {
         Id = id;
         Name = name;
     }
 
-    public long Id { get; set; }
+    public Guid Id { get; set; }
     public string Name { get; set; }
 }
