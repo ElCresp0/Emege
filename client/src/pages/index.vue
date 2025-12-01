4745862ac4adbf7dd3da53c34d0869f3d82ada27
ElCresp0 <jacky8203@protonmail.com>
2025-12-02 00:04:45 +0100

    grid view

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,8 +1,30 @@
 <template>
-  <div id="most-liked-images"></div>
-  <h1>Most liked images</h1>
-  <p>TODO: image gallery</p>
-
+  <v-container id="root-container">
+    
+    <v-row justify="center">
+      <v-col>
+        <div id="most-liked-images" class="root-content-div">
+          <h1>Most liked images</h1>
+          <p>TODO: image gallery</p>
+        </div>
+      </v-col>
+      
+    </v-row>
+    <v-row justify="space-between">
+      <v-col>
+        <div id="most-followed-users" class="root-content-div">
+          <h1>Most follow users</h1>
+          <p>TODO: users table</p>
+        </div>
+      </v-col>
+      <v-col>
+        <div id="trends" class="root-content-div">
+          <h1>Trends</h1>
+          <p>TODO: trend plots</p>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 
 </template>
 
