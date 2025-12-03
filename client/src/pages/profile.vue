3d1478670ddd0d58c85538f37265cb90c2365583
ElCresp0 <jacky8203@protonmail.com>
2025-12-03 01:16:36 +0100

    actual API communication

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -1,8 +1,21 @@
 <!-- TODO: image gallery, adding images, creating collection, user data and stats -->
 <template>
     <h1>Hi I'm Greg</h1>
+    <p>My user endpoint yields: {{ userInfo }}</p>
 </template>
 
 <script lang="ts" setup>
-  //
+import axios from 'axios';
+
+  const userInfo = ref("wait for it!");
+  let userName = "greg";
+  axios.get(`/api/users/${userName}`)
+    .then(response => {
+      console.info(response.data)
+      userInfo.value = response.data;
+    })
+    .catch(err => {
+      console.error("err:", err);
+    });
+
 </script>
--- server/EmegeAPI/Models/User.cs
+++ server/EmegeAPI/Models/User.cs
@@ -2,12 +2,14 @@ namespace EmegeAPI.Models;
 
 public class UserModel
 {
-    public UserModel(Guid id, string name)
+    public UserModel(Guid id, string name, DateOnly joinDate)
     {
         Id = id;
         Name = name;
+        JoinDate = joinDate;
     }
 
     public Guid Id { get; set; }
     public string Name { get; set; }
+    public DateOnly JoinDate { get; set; }
 }
