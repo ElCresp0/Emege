035ce1c583b4b8fa95b48f857e2f962e6303403b
ElCresp0 <jacky8203@protonmail.com>
2025-12-03 23:22:40 +0100

    hardcoded image library

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -18,7 +18,21 @@
           </p>
         </div>
       </v-col>
-      
+    </v-row>
+
+    <v-row justify="center">
+      <v-col>
+        <div id="user-images" class="root-content-div">
+          <h1>My images</h1>
+           <div class="imageContainer">
+            <v-img :src="images[page - 1].image" height="300px"></v-img>
+            <p>
+              {{ images[page - 1].title }}
+            </p>
+           </div>
+          <v-pagination v-model="page" :length="pages" />
+        </div>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -32,7 +46,7 @@
   let userName = "greg";
   axios.get(`/api/users/${userName}`)
     .then(response => {
-      console.info(response.data)
+      // console.info(response.data)
       userInfo.value = response.data;
     })
     .catch(err => {
@@ -46,4 +60,31 @@
     openOnClick: true,
     openOnHover: false
   };
+
+  // image gallery
+  // TODO: make an Image Template
+  // TODO: fetch images from API
+  const images = ref([
+    {"image": "https://imgs.search.brave.com/4-q0ajKE9OOryXBk7IgDAwJztLfop-J9nmGApbbqIFA/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly90My5m/dGNkbi5uZXQvanBn/LzA1LzY0LzgxLzUy/LzM2MF9GXzU2NDgx/NTI0OV9MZXBTa0w2/NGIwd1JIN3NvajZO/N2RkcDlsQWJMcXNn/cC5qcGc",
+      "title": "scatter"
+    },
+    {"image": "https://imgs.search.brave.com/3f7npJL7ntlUB4JqGvC0cm2H_u_lKiYQKnKKHmWqhwI/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wYW5k/YXMucHlkYXRhLm9y/Zy9kb2NzL19pbWFn/ZXMvcGFuZGFzLURh/dGFGcmFtZS1wbG90/LTIucG5n",
+      "title": "chart"
+    },
+    {"image": "https://imgs.search.brave.com/o5O1kczzAI6f25nswrCkX7Pz7PlAt531FWANx3IdOWU/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9wYW5k/YXMucHlkYXRhLm9y/Zy9kb2NzL19pbWFn/ZXMvcGFuZGFzLURh/dGFGcmFtZS1wbG90/LTEucG5n",
+      "title": "bars"
+    }
+  ]);
+  const page = ref(1);
+  const pages = ref(images.value.length);
+
+  // axios.get(`/api/users/${userName}/imageCount`)
+  //   .then(response => {
+  //     // console.info(response.data, response.data["imageCount"]);
+  //     // pages.value = response.data["imageCount"];
+  //   })
+  //   .catch(err => {
+  //     console.error("err:", err);
+  //   });
+
 </script>
