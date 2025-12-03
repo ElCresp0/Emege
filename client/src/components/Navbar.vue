8ed7e162af672d8c2e1d3a429b3dd2b5ac7056aa
ElCresp0 <jacky8203@protonmail.com>
2025-12-03 19:10:04 +0100

    add tooltips

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,7 +1,16 @@
 <template>
 <v-app-bar :elevation="2">
   <template v-slot:prepend>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-tooltip
+        :open-on-hover="false"
+        :persistent="false"
+        location="bottom"
+        open-on-click>
+      <template v-slot:activator="{ props }">
+        <v-app-bar-nav-icon v-bind="props"></v-app-bar-nav-icon>
+      </template>
+      <span>TODO: expandable menu</span>
+    </v-tooltip>
   </template>
 
   <v-app-bar-title><RouterLink to="/">Emege</RouterLink></v-app-bar-title>
@@ -11,12 +20,20 @@
     <RouterLink to="/profile">Profile</RouterLink>
 
     <!-- TODO: implement user search -->
-    <v-btn icon="mdi-magnify"></v-btn>
+    <v-btn icon="mdi-magnify" v-tooltip="searchUsersTooltip"></v-btn>
   </template>
 </v-app-bar>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+  import { RouterLink } from 'vue-router';
 
+  // tooltip definition
+  const searchUsersTooltip = {
+    text: "TODO: search users",
+    location: "bottom",
+    persistent: false,
+    openOnClick: true,
+    openOnHover: false
+  };
 </script>
--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -1,13 +1,34 @@
 <!-- TODO: image gallery, adding images, creating collection, user data and stats -->
 <template>
-    <h1>Hi I'm Greg</h1>
-    <p>My user endpoint yields: {{ userInfo }}</p>
+  <v-container id="root-container">
+    
+    <v-row justify="center">
+      <v-col>
+        <div id="profile-header" class="root-content-div">
+          <!-- TODO: markdown bio editor
+           https://github.com/DCsunset/vuetify-markdown-editor -->
+          <h1>Hi I'm Greg</h1>
+          <p>
+            My user endpoint yields: {{ userInfo }} 
+            <v-btn
+              variant="text"
+              icon="mdi-square-edit-outline"
+              v-tooltip:="editBioToolip">
+            </v-btn>
+          </p>
+        </div>
+      </v-col>
+      
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-import axios from 'axios';
+  import axios from 'axios';
 
   const userInfo = ref("wait for it!");
+
+  // fetch user info from backend
   let userName = "greg";
   axios.get(`/api/users/${userName}`)
     .then(response => {
@@ -18,4 +39,11 @@ import axios from 'axios';
       console.error("err:", err);
     });
 
+  // tooltip definition
+  const editBioToolip = {
+    text: "TODO: edit bio",
+    persistent: false,
+    openOnClick: true,
+    openOnHover: false
+  };
 </script>
